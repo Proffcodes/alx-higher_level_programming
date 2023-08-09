6b0506f27966a54393b53b14dce69c855bfac658
proffcodes <olalekan.paul2010@gmail.com>
2023-08-09 06:24:27 -0700

    completed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3)
-for i in range(1, 10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i < 9 else "\n")
+#!/usr/bin/python3
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
